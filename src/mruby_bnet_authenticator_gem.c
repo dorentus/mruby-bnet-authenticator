4d93888667c2dbc20dd81708a8586f33b0bca179
Zhang Yi <zhangyi.cn@gmail.com>
2014-03-28 22:24:37 +0800

    fix compiler warnings

--- src/mruby_bnet_authenticator_gem.c
+++ src/mruby_bnet_authenticator_gem.c
@@ -10,14 +10,18 @@ static mrb_value
 mrb_mod_exp(mrb_state *mrb, mrb_value self)
 {
   mrb_value value, exp, mod;
+  const char *v_str, *exp_str, *mod_str;
+  BIGD v_bd, exp_bd, mod_bd, result_bd;
+  size_t nchars;
+  char *result_str;
+  mrb_value result;
+
   mrb_get_args(mrb, "SSS", &value, &exp, &mod);
 
-  const char *v_str, *exp_str, *mod_str;
   v_str = mrb_string_value_cstr(mrb, &value);
   exp_str = mrb_string_value_cstr(mrb, &exp);
   mod_str = mrb_string_value_cstr(mrb, &mod);
 
-  BIGD v_bd, exp_bd, mod_bd, result_bd;
   v_bd = bdNew();
   exp_bd = bdNew();
   mod_bd = bdNew();
@@ -33,13 +37,12 @@ mrb_mod_exp(mrb_state *mrb, mrb_value self)
   bdFree(&exp_bd);
   bdFree(&mod_bd);
 
-  size_t nchars = bdConvToHex(result_bd, NULL, 0);
-  char *result_str = malloc(nchars + 1);
+  nchars = bdConvToHex(result_bd, NULL, 0);
+  result_str = malloc(nchars + 1);
   bdConvToHex(result_bd, result_str, nchars + 1);
 
   bdFree(&result_bd);
 
-  mrb_value result;
   result = mrb_str_new_cstr(mrb, result_str);
 
   free(result_str);
@@ -51,13 +54,13 @@ mrb_mod_exp(mrb_state *mrb, mrb_value self)
 static mrb_value
 mrb_uint32_split_to_uint16_be(mrb_state *mrb, mrb_value self)
 {
-  uint32_t input = 0;
+  uint32_t input;
+  mrb_value list[2];
+
   mrb_get_args(mrb, "i", &input);
 
-  uint32_t output0 = (input & 0xffff0000) >> 16;
-  uint32_t output1 = (input & 0x0000ffff) >> 0;
-
-  mrb_value list[2] = {mrb_fixnum_value(output0), mrb_fixnum_value(output1)};
+  list[0] = mrb_fixnum_value((input & 0xffff0000) >> 16);
+  list[1] = mrb_fixnum_value((input & 0x0000ffff) >> 0);
 
   return mrb_ary_new_from_values(mrb, 2, list);
 }
@@ -66,8 +69,11 @@ mrb_uint32_split_to_uint16_be(mrb_state *mrb, mrb_value self)
 void
 mrb_mruby_bnet_authenticator_gem_init(mrb_state* mrb)
 {
-  struct RClass *bnet_module = mrb_define_module(mrb, "Bnet");
-  struct RClass *util_class = mrb_define_module_under(mrb, bnet_module, "Util");
+  struct RClass *bnet_module, *util_class;
+
+  bnet_module = mrb_define_module(mrb, "Bnet");
+  util_class = mrb_define_module_under(mrb, bnet_module, "Util");
+
   mrb_define_class_method(mrb, util_class, "mod_exp", mrb_mod_exp, MRB_ARGS_REQ(3));
 
 #ifndef MRB_INT16
